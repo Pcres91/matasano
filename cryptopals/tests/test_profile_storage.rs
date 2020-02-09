01859ab9e9efe0ec86507956ed168cb052dc1a57
Paul <paulcresswell@live.com>
2020-02-09 17:05:02 +0000

    Got tests to succeed again

--- cryptopals/tests/test_profile_storage.rs
+++ cryptopals/tests/test_profile_storage.rs
@@ -1,11 +1,13 @@
 extern crate crypto;
 
+use crypto::aes;
 use crypto::user_storage::*;
 
 #[test]
 fn test_adding_a_profile() {
     unsafe {
-        let _out = profile_for("test@tester.com");
+        let out = profile_for("test@tester.com").unwrap();
+        PROFILE_STORAGE.add_from_hash(&out).unwrap();
         assert_eq!(PROFILE_STORAGE.profiles.len(), 1);
         let user_prof = &PROFILE_STORAGE.profiles[0];
         assert_eq!(user_prof.email, "test@tester.com");
@@ -18,9 +20,10 @@ fn test_adding_a_profile() {
 fn test_return_from_profile_for() {
     unsafe {
         let out = profile_for("next@tester.com").unwrap();
+        use std::str::from_utf8;
         assert_eq!(
-            out,
-            format!("email=next@tester.com&uid={}&role=User", NEXT_UID - 1)
+            from_utf8(&aes::decrypt_ecb_128(&out, &RND_KEY).unwrap()).unwrap(),
+            "email=next@tester.com&uid=0&role=User"
         );
     }
 }
